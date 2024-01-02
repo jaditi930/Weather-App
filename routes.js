414b9492e2df9c98d97d819320ffcd20d6d1f897
Aditi Jain <jaditi930@gmail.com>
2024-01-03 02:40:04 +0530

    endpoints created

--- routes.js
+++ routes.js
@@ -1,11 +1,19 @@
 const express=require("express");
+const asyncHandler=require("express-async-handler")
 const router=express.Router();
 
-function home(){
-    return {"message":"Api working fine"}
-}
+const home=asyncHandler(async (req,res)=>{
+    res.status(200).send({"message":"Api working fine"})
+})
+
+const getWeather=asyncHandler(async (req,res)=>{
+    let body=req.body
+    console.log(body)
+    res.status(200).send({"weather":""})
+})
 
 router.get("/home",home)
+router.post("/getWeather",getWeather)
 
 
 module.exports=router;
